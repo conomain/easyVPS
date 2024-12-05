f58cf020ec7bc387ac089f3852f01015c707e3a1
Vladimir Fedoseev <fedosvla@fit.cvut.cz>
2024-12-05 18:48:41 +0100

    Project init

---
+++ src/main/java/cz/cvut/fit/tjv/easyvps/EasyvpsApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.easyvps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyvpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyvpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/cvut/fit/tjv/easyvps/EasyvpsApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.easyvps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyvpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
